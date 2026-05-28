006f0d50e4608f44cea12ef3edec535b2d65f084
MatiasPalacios5 <matiaspalacios656@gmail.com>
2026-05-28 17:28:47 -0300

    chore: limpio repo, actualizo .gitignore y elimino target/

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -1,31 +1,22 @@
--- Elimina la tabla 'users' si ya existe para asegurar un inicio limpio
-DROP TABLE IF EXISTS users;
-
--- Crea la tabla 'users' con los campos originales, adaptados para SQLite
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, -- Clave primaria autoincremental para SQLite
-    name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
-    password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
 );
 
-DROP TABLE IF EXISTS persons;
-
-CREATE TABLE persons(
+CREATE TABLE persons (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     dni INTEGER UNIQUE NOT NULL,
     name TEXT NOT NULL,
     apellido TEXT NOT NULL
 );
 
-DROP TABLE IF EXISTS teachers;
-
 CREATE TABLE teachers (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     id_person INTEGER NOT NULL,
     career TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
-
-    CONSTRAINT fk_id FOREIGN KEY (id_person) 
-    REFERENCES persons(id) 
+    CONSTRAINT fk_id FOREIGN KEY (id_person)
+    REFERENCES persons(id)
     ON DELETE CASCADE
 );
